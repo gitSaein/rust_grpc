1c65f9bcfb3803610cafa7b450e7ec1754a38f1e
saeinlee <gitsaein@gmail.com>
2021-11-06 16:50:21 +0900

    readin

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rust_grpc
+## Cargo
+- Rust build system
+
+
+
+
+### references
+- https://doc.rust-lang.org/stable/book/ch01-03-hello-cargo.html
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("start...");
+
+    let mut guess = String::new(); // mut: 가변 변수
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
 }
