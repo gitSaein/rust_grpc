adeb73b1477c9ecb719efacee8e97c0c2265d4d7
saeinlee <gitsaein@gmail.com>
2021-11-07 19:36:05 +0900

    rand func

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("start...");
+    println!("guess number");
+    let mut rng = rand::thread_rng();
 
-    let mut guess = String::new(); // mut: 가변 변수
+    // let mut guess = String::new();
+    // io::stdin().read_line(&mut guess)
+    //     .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
+    println!("rand number: {}", rng.gen_range(0..10));
 
 }
